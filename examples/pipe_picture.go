e10fde67f21ca641e6d582a08db959c896222e26
Hampus Dunström <hampus@hejare.se>
2020-12-02 11:42:45 +0100

    WIP: Add pipeline picture example

---
+++ examples/pipe_picture.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"time"
+
+	"github.com/Dunstrom/gst"
+	"github.com/lijo-jose/glib"
+)
+
+func main() {
+	pipeline, err := gst.ParseLaunch("filesrc location=images/logo.png ! decodebin ! imagefreeze ! tee name=t ! autovideosink t. ! queue ! fakesink name=fakesink enable-last-sample=1")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to create pipeline: ", err)
+		os.Exit(1)
+	}
+	if pipeline.SetState(gst.STATE_PLAYING) == gst.STATE_CHANGE_FAILURE {
+		fmt.Fprintln(os.Stderr, "Failed to start pipeline")
+		os.Exit(1)
+	}
+
+	go func() {
+		i := 0
+		for {
+			time.Sleep(time.Second * 2)
+			filename := fmt.Sprintf("pipe_picture_%d.jpg", i)
+			picturePipeline, err := gst.ParseLaunch(fmt.Sprintf("appsrc name=appsrc ! jpegenc ! filesink location=%s", filename))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to create pipeline: ", err)
+				os.Exit(1)
+			}
+			fakeSink := pipeline.GetByName("fakesink")
+			//appSrc := &gst.AppSrc{picturePipeline.GetByName("appsrc")}
+			if picturePipeline.SetState(gst.STATE_PLAYING) == gst.STATE_CHANGE_FAILURE {
+				fmt.Fprintln(os.Stderr, "Failed to start picture pipeline")
+				os.Exit(1)
+			}
+			sample := fakeSink.GetProperty("last-sample")
+			if sample == nil {
+				fmt.Fprintln(os.Stderr, "Failed to pull sample from fakesink")
+			}
+			fmt.Fprintln(os.Stdout, "Sample type: ", reflect.TypeOf(sample))
+			fmt.Fprintln(os.Stdout, "sample: ", sample)
+			//appSrc.PushBuffer(sample.GetBuffer())
+			i += 1
+			time.Sleep(time.Second * 8)
+		}
+	}()
+	glib.NewMainLoop(nil).Run()
+}
