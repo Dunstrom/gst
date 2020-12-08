06cdba73d182fd22c4a12d6dbbfaa8764fe5747d
Hampus Dunström <hampus@hejare.se>
2020-12-08 21:15:51 +0100

    WIP: Trying out a lot of stuff...

--- examples/pipe_picture.go
+++ examples/pipe_picture.go
@@ -4,13 +4,51 @@ import "C"
 import (
 	"fmt"
 	"github.com/Dunstrom/gst"
-	"github.com/ziutek/glib"
 	"os"
 	"time"
 )
 
+func takePicture(pipeline *gst.Pipeline, filename string) {
+	fmt.Fprintln(os.Stdout, "Setting up a picture pipeline")
+	picturePipeline, err := gst.ParseLaunch(fmt.Sprintf("appsrc name=appsrc num-buffers=1 ! jpegenc snapshot=1 ! filesink location=%s", filename))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to create pipeline: ", err)
+		os.Exit(1)
+	}
+	fakeSink := &gst.Sink{pipeline.GetByName("fakesink")}
+	appSrc := &gst.AppSrc{picturePipeline.GetByName("appsrc")}
+	if picturePipeline.SetState(gst.STATE_PLAYING) == gst.STATE_CHANGE_FAILURE {
+		fmt.Fprintln(os.Stderr, "Failed to start picture pipeline")
+		os.Exit(1)
+	}
+	fmt.Fprintln(os.Stdout, "Started the picture pipeline")
+	time.Sleep(time.Second * 2)
+	for i := 0; i < 10; i += 1 {
+		fmt.Fprintln(os.Stdout, "Pulling a sample")
+		sample := fakeSink.GetLastSample()
+		if sample == nil {
+			fmt.Fprintln(os.Stderr, "Failed to pull sample from fakesink")
+			return
+		}
+		fmt.Fprintln(os.Stdout, "Pushing the sample")
+		buffer := sample.GetBuffer().DeepCopy()
+		fmt.Fprintf(os.Stdout, "Size of buffer in sample %v\n", buffer.GetSize())
+		appSrc.PushBuffer(buffer)
+		state, _, _ := pipeline.GetState(1000)
+		switch state {
+		case gst.STATE_PLAYING:
+			fmt.Fprintln(os.Stdout, "pipeline in playing state")
+		case gst.STATE_PAUSED:
+			fmt.Fprintln(os.Stdout, "pipeline in paused state")
+		default:
+			fmt.Fprintf(os.Stdout, "unknown state %v\n", state)
+		}
+	}
+	fmt.Fprintf(os.Stdout, "Took picture of pipeline stored at %s\n", filename)
+}
+
 func main() {
-	pipeline, err := gst.ParseLaunch("filesrc location=images/logo.png ! decodebin ! imagefreeze ! fakesink name=fakesink enable-last-sample=1") // t. ! queue ! videoflip method=clockwise ! autovideosink") //")
+	pipeline, err := gst.ParseLaunch("videotestsrc ! fakesink name=fakesink enable-last-sample=1") // video/x-raw, framerate=(fraction)5/1 ! fakesink name=fakesink enable-last-sample=1") // t. ! queue ! videoflip method=clockwise ! autovideosink") //")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to create pipeline: ", err)
 		os.Exit(1)
@@ -19,32 +57,5 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Failed to start pipeline")
 		os.Exit(1)
 	}
-	go func() {
-		i := 0
-		for {
-			filename := fmt.Sprintf("pipe_picture_%d.jpg", i)
-			picturePipeline, err := gst.ParseLaunch(fmt.Sprintf("appsrc name=appsrc ! jpegenc ! filesink location=%s", filename))
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Failed to create pipeline: ", err)
-				os.Exit(1)
-			}
-
-			fakeSink := &gst.Sink{pipeline.GetByName("fakesink")}
-			appSrc := &gst.AppSrc{picturePipeline.GetByName("appsrc")}
-			if picturePipeline.SetState(gst.STATE_PLAYING) == gst.STATE_CHANGE_FAILURE {
-				fmt.Fprintln(os.Stderr, "Failed to start picture pipeline")
-				os.Exit(1)
-			}
-			time.Sleep(time.Second * 2)
-			sample := fakeSink.GetLastSample()
-			if sample == nil {
-				fmt.Fprintln(os.Stderr, "Failed to pull sample from fakesink")
-				continue
-			}
-			appSrc.PushBuffer(sample.GetBuffer())
-			i += 1
-			time.Sleep(time.Second * 8)
-		}
-	}()
-	glib.NewMainLoop(nil).Run()
+	takePicture(pipeline, "picture.jpg")
 }
