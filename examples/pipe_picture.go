966c0013daccf21b2aba6b7df9987cb94f24ccff
Hampus Dunström <hampus@hejare.se>
2020-12-10 10:27:42 +0100

    WIP: Found issue in gst debug output that the appsrc input  buffer were exceeded so set it to unlimited

--- examples/pipe_picture.go
+++ examples/pipe_picture.go
@@ -10,7 +10,7 @@ import (
 
 func takePicture(pipeline *gst.Pipeline, filename string) {
 	fmt.Fprintln(os.Stdout, "Setting up a picture pipeline")
-	picturePipeline, err := gst.ParseLaunch(fmt.Sprintf("appsrc name=appsrc ! videoconvert !video/x-raw, format=(string)RGB, width=(int)320, height=(int)240, framerate=(fraction)30/1, multiview-mode=(string)mono, pixel-aspect-ratio=(fraction)1/1, interlace-mode=(string)progressive ! pngenc snapshot=1 ! filesink location=%s", filename))
+	picturePipeline, err := gst.ParseLaunch(fmt.Sprintf("appsrc name=appsrc max-bytes=0 ! videoconvert !video/x-raw, format=(string)RGB, width=(int)320, height=(int)240, framerate=(fraction)30/1, multiview-mode=(string)mono, pixel-aspect-ratio=(fraction)1/1, interlace-mode=(string)progressive ! pngenc snapshot=1 ! filesink location=%s", filename))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to create pipeline: ", err)
 		os.Exit(1)
@@ -23,15 +23,13 @@ func takePicture(pipeline *gst.Pipeline, filename string) {
 	}
 	fmt.Fprintln(os.Stdout, "Started the picture pipeline")
 	time.Sleep(time.Second * 2)
-	for i := 0; i < 5; i += 1 {
+	for i := 0; i < 10; i += 1 {
 		fmt.Fprintln(os.Stdout, "Pulling a sample")
 		sample := fakeSink.GetLastSample()
 		fmt.Fprintf(os.Stdout, "Sample caps: %s\n", sample.GetCaps().String())
 		bufferList := sample.GetBufferList()
-		if bufferList == nil {
-			fmt.Fprintln(os.Stderr, "No buffer list in sample")
-		} else {
-			fmt.Fprintf(os.Stdout, "Sample buffer list length: %d\n", bufferList.Length())
+		if bufferList != nil {
+			fmt.Fprintf(os.Stdout, "Found sample buffer list with length: %d in sample\n", bufferList.Length())
 		}
 
 		if sample == nil {
